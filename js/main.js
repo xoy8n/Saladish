9a1ad0abea09d4ff72ce79b1e797fcaadaa2f8e5
xoy8n <s01057111917@gmail.com>
2022-04-25 15:58:01 +0900

    #2.5 about-us

--- js/main.js
+++ js/main.js
@@ -9,6 +9,16 @@ menuToggler.addEventListener("click", function () {
   body.classList.toggle("open");
 });
 
+//nav menu active
+const currnetLocation = location.href;
+const menuItem = document.querySelectorAll("a");
+const menuLength = menuItem.length;
+for (let i = 0; i < menuLength; i++) {
+  if (menuItem[i].href === currnetLocation) {
+    menuItem[i].className = "active";
+  }
+}
+
 //scroll reveal
 window.sr = ScrollReveal();
 
@@ -22,17 +32,42 @@ sr.reveal(".animate-right", {
   origin: "right",
   duration: 1000,
   distance: "25rem",
-  delay: 600,
+  delay: 500,
 });
 sr.reveal(".animate-top", {
   origin: "top",
   duration: 1000,
   distance: "25rem",
-  delay: 600,
+  delay: 300,
 });
 sr.reveal(".animate-bottom", {
   origin: "bottom",
   duration: 1000,
   distance: "25rem",
-  delay: 600,
+  delay: 350,
+});
+
+sr.reveal(".lin1", {
+  origin: "left",
+  duration: 1000,
+  distance: "25rem",
+  delay: 300,
+});
+sr.reveal(".lin2", {
+  origin: "left",
+  duration: 1000,
+  distance: "25rem",
+  delay: 500,
+});
+sr.reveal(".lin3", {
+  origin: "left",
+  duration: 1000,
+  distance: "25rem",
+  delay: 700,
+});
+sr.reveal(".lin4", {
+  origin: "left",
+  duration: 1000,
+  distance: "25rem",
+  delay: 900,
 });
